2dffe1d2772712e73690f5272b9a6e0f3e9385a5
izzydevop <izzydevop@gmail.com>
2023-01-03 14:19:59 +0200

    add log util

---
+++ util_log.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "util_log.h"
+
+
+void ulog()
+{
+	printf("using util_log version: %s", UTIL_LOG_VER);
+}
---
+++ util_log.h
@@ -0,0 +1,8 @@
+//#ifndef _UTIL_LOG_
+//#define _UTIL_LOG_
+
+#define UTIL_LOG_VER	"1.0.0"
+
+void ulog();
+
+//#endif
