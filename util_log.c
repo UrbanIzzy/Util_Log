360e500da087ea51f8246e783fb6f0b6366c9380
izzydevop <izzydevop@gmail.com>
2023-01-03 14:54:07 +0200

    new version

--- util_log.c
+++ util_log.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include "util_log.h"
 
-
 void ulog()
 {
 	printf("using util_log version: %s", UTIL_LOG_VER);
--- util_log.h
+++ util_log.h
@@ -1,7 +1,7 @@
 //#ifndef _UTIL_LOG_
 //#define _UTIL_LOG_
 
-#define UTIL_LOG_VER	"1.0.0"
+#define UTIL_LOG_VER	"1.1.0"
 
 void ulog();
 
